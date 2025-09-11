5a97eb515163591f3a75a9cefaedd66d706403aa
chenglingcong <cc781032855@163.com>
2025-09-11 22:27:47 +0800

    homework task2-goroutine

--- task2/goroutine.go
+++ task2/goroutine.go
@@ -23,9 +23,34 @@ func evennum(num []int) {
 	}
 }
 
+func task1() {
+	start := time.Now()
+	fmt.Printf("executing task1\n")
+	time.Sleep(2000 * time.Millisecond)
+	end := time.Now()
+	fmt.Printf("task1 cost %v\n", end.Sub(start))
+}
+func task2() {
+	start := time.Now()
+	fmt.Printf("executing task2\n")
+	time.Sleep(1000 * time.Millisecond)
+	end := time.Now()
+	fmt.Printf("task2 cost %v\n", end.Sub(start))
+}
+func task3() {
+	start := time.Now()
+	fmt.Printf("executing task3\n")
+	time.Sleep(1500 * time.Millisecond)
+	end := time.Now()
+	fmt.Printf("task3 cost %v\n", end.Sub(start))
+}
+
 func main() {
 	num := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	go oddnum(num)
 	go evennum(num)
-	time.Sleep(5000 * time.Millisecond)
+	go task1()
+	go task2()
+	go task3()
+	time.Sleep(10000 * time.Millisecond)
 }
